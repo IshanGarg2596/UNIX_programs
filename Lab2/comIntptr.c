af420d058c5234e49740026073f52dbc003a9239
Ishan  Garg <ishan@Ishans-MacBook-Air.local>
2022-09-11 16:05:52 +0530

    implemented lab2

--- Lab2/comIntptr.c
+++ Lab2/comIntptr.c
@@ -4,6 +4,7 @@
 #include<unistd.h> // Definition for fork() and execvp()
 #include<sys/wait.h> // Definition for wait()
 
+#define MAXCOM 1000 // max no of letters
 #define MAXLIST 100 // max number of commands
 
 
@@ -22,29 +23,44 @@ void parseSpace(char* str, char** parsed)
     }
 }
 
-
-int main(){
-    char inputString[] = "pwd", *parsedArgs[MAXLIST];
-
-    parseSpace(inputString, parsedArgs);
-
-
-    // System Command execution starts here
+void execArgs(char** parsed)
+{
     // Forking a child
     pid_t pid = fork(); 
   
     if (pid == -1) {
         printf("\nFailed forking child..");
-        return 0;
+        return;
     } else if (pid == 0) {
-        if (execvp(parsedArgs[0], parsedArgs) < 0) {
+        if (execvp(parsed[0], parsed) < 0) {
             printf("\nCould not execute command..");
         }
         exit(0);
     } else {
         // waiting for child to terminate
         wait(NULL); 
+        return;
     }
+}
+
+int main(){
+    char inputString[MAXCOM], *parsedArgs[MAXLIST];
+
+    
+
+    while(1){
+        printf("\n $ ");
+        scanf("%[^\n]s", inputString);
+
+        if(strcmp(inputString, "") == 0)
+            continue;
+
+        parseSpace(inputString, parsedArgs);
+
+        execArgs(parsedArgs);
+    }
+
+    
 
     return 0;
 }
--- ls.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-//Used for handling directory files
-#include <dirent.h>
-//For EXIT codes and error handling
-#include <errno.h>
-#include <stdlib.h>
